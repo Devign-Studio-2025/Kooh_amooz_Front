aed50c796181a0d4faae349e92bcc31c8b35027a
Mohsen Pourdehghan <mohsen.purdehghan@gmail.com>
2025-10-12 19:32:25 +0330

    feat: newest courses section complete

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+import "../libs/bootstrap/bootstrap.bundle.min.js";
+
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
