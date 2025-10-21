694998aeb605fee1db685971b6b829513ad97afc
Mohsen Pourdehghan <mohsen.purdehghan@gmail.com>
2025-10-21 13:50:34 +0330

    feat: course page complete

--- assets/js/components/skeleton.js
+++ assets/js/components/skeleton.js
@@ -4,11 +4,37 @@ function initSkeleton() {
   const mainElements = document.querySelectorAll(".skeleton-after");
 
   skeletons.forEach((el) => {
-    el.remove();
+    el.classList.add("skeleton-before--hide");
   });
 
   mainElements.forEach((el) => {
-    el.classList.remove("skeleton-after");
+    el.classList.add("skeleton-after--show");
+  });
+}
+
+export function startSkeleton() {
+  const skeletons = document.querySelectorAll(".skeleton-before");
+  const mainElements = document.querySelectorAll(".skeleton-after");
+
+  skeletons.forEach((el) => {
+    el.classList.remove("skeleton-before--hide");
+  });
+
+  mainElements.forEach((el) => {
+    el.classList.remove("skeleton-after--show");
+  });
+}
+
+export function endSkeleton() {
+  const skeletons = document.querySelectorAll(".skeleton-before");
+  const mainElements = document.querySelectorAll(".skeleton-after");
+
+  skeletons.forEach((el) => {
+    el.classList.add("skeleton-before--hide");
+  });
+
+  mainElements.forEach((el) => {
+    el.classList.add("skeleton-after--show");
   });
 }
 
