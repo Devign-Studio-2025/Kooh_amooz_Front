550d85f2afc9c16a0fdf20b65f13b722cc9a5edc
Mohsen Pourdehghan <mohsen.purdehghan@gmail.com>
2025-10-27 18:57:52 +0330

    before pagination classes change

---
+++ assets/js/pages/user-panel/user_payments.js
@@ -0,0 +1,24 @@
+const table = new DataTable("#payments-table", {
+  language: {
+    url: "assets/libs/datatable/fa.json",
+  },
+  dom: "tpi",
+});
+
+// Search in table
+$(".content-section .input__field").on("keyup", function () {
+  table.search(this.value).draw();
+});
+
+// Select dropdown
+$("#row-length-select").select2({
+  minimumResultsForSearch: Infinity,
+  placeholder: "تعداد سطرها",
+});
+
+// Dropdown set row length
+$("#row-length-select").on("select2:select", function (e) {
+  var data = e.params.data.id;
+
+  table.page.len(data).draw();
+});
